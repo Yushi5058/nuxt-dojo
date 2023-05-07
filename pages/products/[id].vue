ad11809ac9d53ac77b5952f88394550a91a0392e
Yushi_61 <108335997+Yushi5058@users.noreply.github.com>
2023-05-07 10:14:57 +0100

    add route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat nobis laborum modi ut illum eos aspernatur harum? Tenetur, facilis veniam!</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+
+</script>
+
+<style scoped>
+
+</style>
